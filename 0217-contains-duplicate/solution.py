ed5a34a4af9653144b04575a1972801d8e60c295
Antra-Agarwal <antraagarwal2020@gmail.com>
2026-03-30 07:39:53 +0000

    Sync LeetCode submission Runtime - 84 ms (6.66%), Memory - 20.3 MB (93.82%)

---
+++ 0217-contains-duplicate/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        nums.sort()
+
+        for i in range (len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+        
+        return False
+
+nums = [1,2,3,1]
+obj = Solution()
+print (obj.containsDuplicate(nums))
+        
+        
