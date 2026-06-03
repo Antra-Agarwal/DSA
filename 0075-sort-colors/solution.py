6d6fb76311b7b0c77462f17b0239a47fa36106a6
Antra-Agarwal <antraagarwal2020@gmail.com>
2026-06-03 14:11:34 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.3 MB (54.40%)

---
+++ 0075-sort-colors/solution.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def sortColors(self, nums):
+        
+        count0 = 0
+        count1 = 0
+        count2 = 0
+        i = 0
+        
+        for num in nums:
+            if num == 0:
+                count0 += 1
+            elif num == 1:
+                count1 += 1
+            else :
+                count2 += 1
+        
+        
+        while count0:
+            nums[i] = 0
+            count0 -= 1
+            i += 1
+
+        while count1:
+            nums[i] = 1
+            count1 -= 1
+            i += 1
+        
+        while count2:
+            nums[i] = 2
+            count2 -= 1
+            i += 1
+
+
+        
