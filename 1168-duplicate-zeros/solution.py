3403b7b73bbc96e20588358f07bc7ecd3bffa2b7
Antra-Agarwal <antraagarwal2020@gmail.com>
2026-06-06 04:58:47 +0000

    Sync LeetCode submission Runtime - 507 ms (5.53%), Memory - 19.4 MB (61.48%)

---
+++ 1168-duplicate-zeros/solution.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        n = len(arr)
+        i = 0
+
+        while i < n:
+            if arr[i] == 0:
+
+                for j in range(n - 1, i, -1):
+                    arr[j] = arr[j - 1]
+
+                if i + 1 < n:
+                    arr[i + 1] = 0
+
+                i += 1      
+
+            i += 1
+        
