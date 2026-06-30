5ee20d75b308fb012de52ed12941e1016aada6c3
Antra-Agarwal <antraagarwal2020@gmail.com>
2026-06-30 06:35:04 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.5 MB (80.64%)

--- 0033-search-in-rotated-sorted-array/solution.py
+++ 0033-search-in-rotated-sorted-array/solution.py
@@ -1,12 +1,29 @@
 class Solution(object):
     def search(self, nums, target):
         n = len(nums)
-        ans = -1
+        left = 0
+        right = n-1
 
-        for i in range(n):
-            if nums[i] == target :
-                ans = i
+        while left <= right :
+            mid = left + (right-left)//2
+
+            if nums[mid] == target :
+                return mid
+            
+            # Left half is sorted 
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid -1 
+                else :
+                    left = mid +1
+            
+            # right half is sorted
+
+            else :
+                if nums[mid] < target <= nums[right]:
+                    left = mid +1
+                else :
+                    right = mid -1
         
-        return ans
-                
+        return -1
         
